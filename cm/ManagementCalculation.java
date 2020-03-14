1a115f97984d569d471a92dde07cdbcdaf01e8a9
Fran Hall <fhall92@gmail.com>
2020-03-14 16:23:06 +0000

    Fixed an error where ManagementCalculation was calculating by Student rate specification

--- cm/ManagementCalculation.java
+++ cm/ManagementCalculation.java
@@ -5,15 +5,14 @@ import java.math.BigDecimal;
 public class ManagementCalculation {
 
     BigDecimal calculation(BigDecimal baseCost){
-        BigDecimal normalPayLimit = new BigDecimal("5.50");
-        BigDecimal reduction = new BigDecimal("0.75");
-        //If less than 5.50, return baseCost
-        if (baseCost.compareTo(normalPayLimit) <= 0) {
-            return baseCost.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+        //If base cost is less than 3, payment should be 3
+        if (baseCost.compareTo(new BigDecimal(3)) <= 0) {
+            BigDecimal cost = new BigDecimal(3);
+            return cost.setScale(2, BigDecimal.ROUND_HALF_EVEN);
         }
-        //Else, return the discounted price at 25% discount above 5.50
+        //Otherwise, require regular payment
         else {
-            return baseCost.subtract(normalPayLimit).multiply(reduction).add(normalPayLimit).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+            return baseCost.setScale(2, BigDecimal.ROUND_HALF_EVEN);
         }
     }
 
